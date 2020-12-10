b35260826a9b2a7994cacbb7fffa87531b0ff6db
Abhijit Valvekar <abhijit.valvekar@gmail.com>
2020-12-10 16:03:34 +0530

    delete

--- src/test/java/com/sample/utils/Constant.java
+++
@@ -1,9 +0,0 @@
-package com.sample.utils;
-
-public class Constant {
-
-	public static final String TEST_PROPERTIES = "./src/test/resources/conf/config.properties";
-	
-	public static final String SAMPLE_DATA = "./src/test/resources/testdata/sampledata.conf";
-
-}
